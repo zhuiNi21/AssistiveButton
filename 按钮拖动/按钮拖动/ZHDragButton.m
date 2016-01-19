2d9e4f8acb6ad4fc923579e88f797f7dd48787c8
周豪 <zhouhao@jingdizhiwa.cn>
2016-01-19 18:07:54 +0800

    add new files2

--- 按钮拖动/按钮拖动/ZHDragButton.m
+++ 按钮拖动/按钮拖动/ZHDragButton.m
@@ -95,9 +95,44 @@
     {
 
     }
-
-    
-    
 }
-
+-(void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint EndPoint = [touch locationInView:self];
+    float offsetX = EndPoint.x - beginPoint.x;
+    float offsetY = EndPoint.y - beginPoint.y;
+    
+    if (self.center.x + offsetX >= SrceenW*ZHDragScale/2) {
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:ZHDragAnimationDuration];
+        self.center = CGPointMake(SrceenW-self.bounds.size.width*ZHDragScale/2,self.center.y + offsetY);
+        
+        [UIView commitAnimations];
+    }else
+    {
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:ZHDragAnimationDuration];
+        self.center = CGPointMake(self.bounds.size.width*ZHDragScale/2,self.center.y + offsetY);
+        
+        [UIView commitAnimations];
+    }
+    if (self.center.y + offsetY >= SrceenH-self.bounds.size.height*ZHDragScale/2) {
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:ZHDragAnimationDuration];
+        self.center = CGPointMake(self.center.x,SrceenH-self.bounds.size.height*ZHDragScale/2);
+        
+        [UIView commitAnimations];
+    }else if(self.center.y + offsetY < self.bounds.size.height*ZHDragScale/2)
+    {
+        [UIView beginAnimations:nil context:nil];
+        [UIView setAnimationDuration:ZHDragAnimationDuration];
+        self.center = CGPointMake(self.center.x,self.bounds.size.height*ZHDragScale/2);
+        
+        [UIView commitAnimations];
+    }else
+    {
+        
+    }
+}
 @end
